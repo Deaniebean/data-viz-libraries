d65ba321620813a8fbdf3edd94b32f5e893b2301
deaniebean <nadine.eunous@gmail.com>
2025-05-15 17:08:00 +0200

    correct links

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -6,19 +6,19 @@ export const Home = () => {
     <div className='home'>
       <div className="intro">
       <p className="hi">Hi, there<WavingHandIcon/></p>
-      <p>This is a practical study comparing (mostly) JavaScript charting libraries</p>
-      <p>Want to know more? You can find my Bachelorthesis on the topic here</p>
-      <p>The code is available on the GitHub <a href='https://github.com/Deaniebean/data-viz-libraries'>Repo</a> </p>
+      <p>This is a practical study comparing (mostly) JavaScript charting libraries.</p>
+      <p>Want to know more? You can find my Bachelorthesis on the topic here.</p>
+      <p>The code is available on the GitHub <a href='https://github.com/Deaniebean/data-viz-libraries'>Repo</a>. </p>
       </div>
       <div className="img-table">
         <h2>Use Cases</h2>
         <div>
           <img src="/data-viz-libraries/linechart.jpg"/>
-          <img src="data-viz-libraries/pareto.jpg"/>
+          <img src="/data-viz-libraries/pareto.jpg"/>
         </div>
         <div>
-        <img src="data-viz-libraries/sankey1.png"/>
-        <img src="data-viz-libraries/sankey2.png"/>
+        <img src="/data-viz-libraries/sankey1.png"/>
+        <img src="/data-viz-libraries/sankey2.png"/>
         </div>
       </div>
     </div>

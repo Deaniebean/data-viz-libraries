4af9bf3a08a9f1dc320be0b2be04740bb8674241
deaniebean <nadine.eunous@gmail.com>
2025-05-22 09:35:02 +0200

    .

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -7,18 +7,18 @@ export const Home = () => {
       <div className="intro">
       <p className="hi">Hi, there<WavingHandIcon/></p>
       <p>This is a practical study comparing (mostly) JavaScript charting libraries.</p>
-      <p>Want to know more? You can find my Bachelorthesis on the topic <a href='/data-viz-libraries/Bachelorarbeit.pdf'>here</a>.</p>
-      <p>The code is available on the GitHub <a href='https://github.com/Deaniebean/data-viz-libraries'>Repo</a>. </p>
+      <p>Want to know more? You can find my Bachelorthesis on the topic here.</p>
+      <p>The code is available on the GitHub <a href='https://github.com/Deaniebean/data-viz-libraries'>Repo.</a> </p>
       </div>
       <div className="img-table">
         <h2>Use Cases</h2>
         <div>
           <img src="/data-viz-libraries/linechart.jpg"/>
-          <img src="/data-viz-libraries/pareto.jpg"/>
+          <img src="data-viz-libraries/pareto.jpg"/>
         </div>
         <div>
-        <img src="/data-viz-libraries/sankey_1.jpg"/>
-        <img src="/data-viz-libraries/sankey_2.jpg"/>
+        <img src="data-viz-libraries/sankey1.png"/>
+        <img src="data-viz-libraries/sankey2.png"/>
         </div>
       </div>
     </div>
